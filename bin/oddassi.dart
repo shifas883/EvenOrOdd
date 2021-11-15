1ae0036c231170cc49c84c7c587b5cbf1272569b
shifas <shifasmeharnk@gmail.com>
2021-11-15 05:37:21 +0530

    odd update

--- bin/oddassi.dart
+++ bin/oddassi.dart
@@ -7,10 +7,10 @@ void main() {
   var num1=int.parse(a);
 
   if(num1%2==0) {
-    stdout.write("The $num1 Is Even,The number is between 100");
+    stdout.write("$num1 Is Even,The number is between 100");
   }
    else
-    print("The $num1 Is Odd,The number is between 100");
+    print("$num1 Is Odd,The number is between 100");
 
 
 }
